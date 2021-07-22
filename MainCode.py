642d9053fc8802ec438c98053288c407072bfcc8
[Oluchi] <oluchifeze22@gmail.com>
2021-07-22 16:21:52 -0400

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,4 +1,3 @@
-
 # since keras already has the mnist library, we can import it from there
 
 import keras
@@ -12,6 +11,7 @@ from keras import backend as K
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 print(x_train.shape, y_train.shape)
+
 
 # process the data
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
@@ -29,3 +29,39 @@ x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
+
+
+# create the model
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), met['accuracy'])
+
+
+# train the model by using model.fit() function
+# this function takes the training data, validation data, epochs and batch size
+# epochs refers to one cycle through the full training dataset. Usually takes a few
+# batch size refers to the number of training examples utilized in one iteration
+# iterations is the number of batches or steps through partitioned packets of the training data, needed to complete one epoch
+
+hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+print("The model has successfully trained")
+
+model.save('mnist.h5')
+print("Saving the model as mnist.h5")
+
+
+# Evaluate the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss: ', score[0])
+print('Test accuracy: ', score[1])
